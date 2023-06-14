57e1901e7c752352560a34826a3269d77e2b667b
Aleksei Kolesnikov <ancientt1k1@gmail.com>
2023-06-14 13:11:08 +0300

    Добавлены UI тесты для кнопок и финального алерта

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,91 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Aleksey Kolesnikov on 13.06.2023.
+//
+
+import XCTest
+
+final class MovieQuizUITests: XCTestCase {
+    private var app: XCUIApplication!
+    
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+        
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+
+    func testYesButton() throws {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["Yes"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+   
+    func testNoButton() throws {
+        sleep(3)
+        
+        let firstPoster = app.images["Poster"]
+        let firstPosterData = firstPoster.screenshot().pngRepresentation
+        
+        app.buttons["No"].tap()
+        sleep(3)
+        
+        let secondPoster = app.images["Poster"]
+        let secondPosterData = secondPoster.screenshot().pngRepresentation
+        let indexLabel = app.staticTexts["Index"]
+        
+        XCTAssertNotEqual(firstPosterData, secondPosterData)
+        XCTAssertEqual(indexLabel.label, "2/10")
+    }
+    
+    func testFinalAlertAdvent() throws {
+        sleep(2)
+        for _ in 0...9 {
+            app.buttons["Yes"].tap()
+            sleep(2)
+        }
+
+        let alert = app.alerts["Этот раунд окончен!"]
+        
+        XCTAssertTrue(alert.exists)
+        XCTAssertTrue(alert.buttons.firstMatch.label == "Сыграть еще раз")
+    }
+    
+    func testAlertButton() throws {
+        sleep(2)
+        for _ in 0...9 {
+            app.buttons["Yes"].tap()
+            sleep(2)
+        }
+        
+        let alert = app.alerts["Этот раунд окончен!"]
+        alert.buttons.firstMatch.tap()
+        
+        sleep(2)
+        
+        XCTAssertFalse(alert.exists)
+        XCTAssertEqual(app.staticTexts["Index"].label, "1/10")
+    }
+}
